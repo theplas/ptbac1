991af1e5180418b875e42e570fd7960a51db7d93
theplas <kanicepakulate@gmail.com>
2020-09-13 10:17:36 +0700

    ptbac1
    ptbac1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace bai1
+{
+    class Program
+  {
+        static void Main(string[] args)
+        {
+             Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console . WriteLine ( " Enter a: " );
+            float a = float.Parse(Console.ReadLine());
+
+            if (a == 0)
+            {
+                Console . WriteLine ( " a must be different from 0 " );
+            }
+            else
+            {
+                Console . WriteLine ( " Enter b: " );
+                float b = float.Parse(Console.ReadLine());
+
+                float x = -b / a;
+                Console.WriteLine("{0}x + {1} = 0 \n=> x = {2}", a, b, x);
+            }
+            Console.ReadKey();
+        }
+    }
+}
